42295f54fb37e8c1f7a4027f1dc755ecc6e204c4
srg <skabassjob@gmail.com>
2022-05-09 01:23:35 +0400

    3.7 add correct routes for error pages

--- src/express/express.js
+++ src/express/express.js
@@ -20,4 +20,7 @@ app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((err, req, res) => res.status(500).render(`errors/500`));
+
 app.listen(DEFAULT_PORT);
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -8,7 +8,5 @@ mainRoutes.get(`/`, (req, res) => res.render(`main`));
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, (req, res) => res.render(`search`));
-mainRoutes.get(`/404`, (req, res) => res.render(`errors/404`));
-mainRoutes.get(`/500`, (req, res) => res.render(`errors/500`));
 
 module.exports = mainRoutes;
