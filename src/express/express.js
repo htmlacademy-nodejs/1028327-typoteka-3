6f00f0ec818088e5dd0327b9730d679122b17539
srg <skabassjob@gmail.com>
2022-05-08 14:26:41 +0400

    3.4 add and connect static files

--- src/express/express.js
+++ src/express/express.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 
 const articlesRoutes = require(`./routes/articles-routes`);
@@ -7,11 +8,14 @@ const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.listen(DEFAULT_PORT);
