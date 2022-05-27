276475e2022d6461fb87f99781f542d75ea77c85
srg <skabassjob@gmail.com>
2022-05-27 14:07:29 +0400

    4.6 set REST API for categories

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -2,15 +2,17 @@
 
 const {Router} = require(`express`);
 const getMockData = require(`../lib/get-mock-data`);
+const category = require(`../api/category`);
+const {
+  CategoryService,
+} = require(`../data-service`);
 
 const app = new Router();
 
 (async () => {
   const mockData = await getMockData();
 
-  app.get(`/`, (req, res) => {
-    res.json(mockData);
-  });
+  category(app, new CategoryService(mockData));
 })();
 
 module.exports = app;
