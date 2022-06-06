7be7664074d52a62477bbd6c3f2cdd2d9a326a41
srg <skabassjob@gmail.com>
2022-06-07 00:52:23 +0400

    4.23 connect service data in my page template

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my`));
+myRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`my`, {articles});
+});
+
 myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 myRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
 
