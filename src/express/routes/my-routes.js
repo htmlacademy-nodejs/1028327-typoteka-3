546072a946d11c6ac0d5cf7c391ebaff9e1ce80d
srg <skabassjob@gmail.com>
2022-06-06 17:38:24 +0400

    4.22 connect service data in main template

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`/my`));
+myRouter.get(`/`, (req, res) => res.render(`my`));
 myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 myRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
 
