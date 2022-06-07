4ecfae1860bfe51fd468d5f531e7289a3cd257bc
srg <skabassjob@gmail.com>
2022-06-07 11:04:34 +0400

    4.24 connect service data in comments template

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -7,10 +7,18 @@ const myRouter = new Router();
 
 myRouter.get(`/`, async (req, res) => {
   const articles = await api.getArticles();
-  res.render(`my`, {articles});
+  const comments = articles.reduce((acc, article) => {
+    acc = [...acc, ...article.comments];
+    return acc;
+  }, []);
+  res.render(`my`, {articles, comments});
 });
 
-myRouter.get(`/comments`, (req, res) => res.render(`comments`));
+myRouter.get(`/comments`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`comments`, {articles});
+});
+
 myRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
 
 module.exports = myRouter;
