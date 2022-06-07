77c905b9bd7cdc48bdc5127abb8cf6adf62ba123
srg <skabassjob@gmail.com>
2022-06-07 11:32:11 +0400

    4.25 connect service data in post-detail template

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const articlesRoutes = new Router();
 
 articlesRoutes.get(`/category/:id`, (req, res) => res.render(`all-categories`));
 articlesRoutes.get(`/add`, (req, res) => res.render(`post`));
 articlesRoutes.get(`/edit/:id`, (req, res) => res.render(`post`));
-articlesRoutes.get(`/:id`, (req, res) => res.render(`post-detail`));
+
+articlesRoutes.get(`/:id`, async (req, res) => {
+  const {id} = req.params;
+  const article = await api.getArticle(id);
+
+  res.render(`post-detail`, {article, categories: article.сategory});
+});
 
 module.exports = articlesRoutes;
