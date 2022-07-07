7162fc558e0576fd2582ba1edccb77b460d6eb8a
srg <skabassjob@gmail.com>
2022-07-07 17:13:29 +0400

    6.5 create models for data

---
+++ src/service/models/article.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const {DataTypes, Model} = require(`sequelize`);
+
+class Article extends Model {}
+
+const define = (sequelize) => Article.init({
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  picture: DataTypes.STRING,
+  announce: {
+    // eslint-disable-next-line new-cap
+    type: DataTypes.STRING(250),
+    allowNull: false,
+  },
+  text: {
+    // eslint-disable-next-line new-cap
+    type: DataTypes.STRING(1000),
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: `Article`,
+  tableName: `articles`,
+  underscored: true,
+});
+
+module.exports = define;
