e5a4eca5d7d301a759461dc5ad73393342d7f66a
srg <skabassjob@gmail.com>
2022-07-19 17:24:59 +0400

    7.10 update validation for article

--- src/service/middlewares/article-validator.js
+++ src/service/middlewares/article-validator.js
@@ -4,24 +4,24 @@ const Joi = require(`joi`);
 const {HttpCode} = require(`../../constants`);
 
 const ErrorOfferMessage = {
-  CATEGORIES_TYPE: `Произошла ошибка при выборе категории объявления`,
-  CATEGORIES_MIN: `Не выбрана ни одна категория объявления`,
+  CATEGORIES: `Не выбрана ни одна категория объявления`,
   TITLE_MIN: `Заголовок содержит меньше 30 символов`,
   TITLE_MAX: `Заголовок не может содержать более 250 символов`,
   ANNOUNCE_MIN: `Анонс содержит меньше 30 символов`,
   ANNOUNCE_MAX: `Анонс не может содержать более 250 символов`,
   TEXT_MAX: `Текст публикации не может содержать более 1000 символов`,
+  DATE: `Произошла ошибка при выборе даты`,
 };
 
 const schema = Joi.object({
   categories: Joi.array().items(
       Joi.number().integer().positive().messages({
-        'number.base': ErrorOfferMessage.CATEGORIES_TYPE,
+        'number.base': ErrorOfferMessage.CATEGORIES,
       }),
-  ).min(1).required().messages({
-    'array.min': ErrorOfferMessage.CATEGORIES_MIN,
+  ).min(1).required(),
+  date: Joi.date().iso().required().messages({
+    'date.base': ErrorOfferMessage.DATE,
   }),
-  date: Joi.date().iso().required(),
   title: Joi.string().min(30).max(250).required().messages({
     'string.min': ErrorOfferMessage.TITLE_MIN,
     'string.max': ErrorOfferMessage.TITLE_MAX,
