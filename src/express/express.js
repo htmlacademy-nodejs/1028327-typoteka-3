4e434cf95487bd44971cbdc1dde7da75f7cb50ab
srg <skabassjob@gmail.com>
2022-07-31 13:51:40 +0400

    7.26 add session for front service

--- src/express/express.js
+++ src/express/express.js
@@ -2,7 +2,9 @@
 
 const path = require(`path`);
 const express = require(`express`);
-
+const session = require(`express-session`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+const sequelize = require(`../service/lib/sequelize`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
@@ -12,7 +14,30 @@ const PUBLIC_DIR = `public`;
 const UPLOAD_DIR = `upload`;
 const FAVICON_DIR = `public/favicon`;
 
+const {SESSION_SECRET} = process.env;
+if (!SESSION_SECRET) {
+  throw new Error(`SESSION_SECRET environment variable is not defined`);
+}
+
 const app = express();
+
+const mySessionStore = new SequelizeStore({
+  db: sequelize,
+  expiration: 180000,
+  checkExpirationInterval: 60000,
+});
+
+sequelize.sync({force: false});
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+  secret: SESSION_SECRET,
+  store: mySessionStore,
+  resave: false,
+  proxy: true,
+  saveUninitialized: false,
+}));
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
@@ -24,7 +49,7 @@ app.use(express.static(path.resolve(__dirname, FAVICON_DIR)));
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
-app.use((req, res) => res.status(400).render(`errors/404`));
-app.use((err, req, res) => res.status(500).render(`errors/500`));
+app.use((_req, res) => res.status(400).render(`errors/404`));
+app.use((_err, _req, res) => res.status(500).render(`errors/500`));
 
 app.listen(FRONT_DEFAULT_PORT);
